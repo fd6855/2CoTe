0e6f743edb187d33d91d7e59b7a79f1d35cb6183
fd6855 <52096014+fd6855@users.noreply.github.com>
2023-11-08 14:45:58 +0000

    1108_greedy
    1108_greedy

---
+++ greedy/AandB.py
@@ -0,0 +1,13 @@
+A = input()
+B = input()
+while len(A) < len(B):
+  if A != B:
+    if B[-1] == "A":
+      B = B[:-1]
+    else:
+      B = B[:-1]
+      B = B[::-1]
+if A == B:
+  print("1")
+else:
+  print("0")
